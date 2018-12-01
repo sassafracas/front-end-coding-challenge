8308d055ca7040b26359beccb88c8aaed7a9ae34
github username <torpid@gmail.com>
2018-12-01 17:09:27 -0500

    cleaned up

--- challenge2.js
+++ challenge2.js
@@ -2,26 +2,24 @@ document.addEventListener("DOMContentLoaded", function (){
 
   let productName = ""
   let productPrice = ""
-  let discount = "15%"
-
-  let swatchLinks = document.querySelectorAll("a.swatchanchor");
-  let sizeLinks = [];
-
   let rightDiv = document.querySelector("div.product-detail-outer")
+  let swatchLinks = document.querySelectorAll("a.swatchanchor")
+  
+  let discount = "15%"
 
   function getAllSizeLinks (){
 
     rightDiv.addEventListener("click", (event) => {
       if (event.target.title.includes("Size") && event.target.parentNode.className.includes("selectable")) {
         if (!event.target.parentNode.className.includes("selected")) {
-          unselect(event)
+          unselect()
           event.target.parentNode.className += " selected"
         }
       console.log(event)
       }
     })
 
-    function unselect (event) {
+    function unselect () {
       swatchLinks.forEach((link) => {
         if (link.title.includes("Size") && link.parentNode.className.includes("selectable") && link.parentNode.className.includes("selected")) {
           link.parentNode.className = "selectable"
